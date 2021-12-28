c0e9fd895bb197ba8c1c52bafe3d7c2ef77669ca
Panda <traitnil@gmail.com>
2021-12-28 19:12:36 +0200

    console.log

---
+++ app.js
@@ -0,0 +1,38 @@
+const a = 5;
+const b = 5; 
+const c = 30;
+const d = 5;  
+const e = 2;
+const f = 8;
+const numb = 33;
+const bool = false;
+const str = "javascript";
+const strNum = "100";
+
+console.log(a * b);
+console.log(c / d);
+console.log(e + f);
+console.log(numb,bool,str,strNum);
+
+let num = 1;
+num++;
+num--;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+
+
+
+
+// number = prompt('Type the number');
+
+// action = prompt('Choose your action');
+
+// if (action === '+') {
+// result = Number(number) + 2;
+// } else {
+// result = Number(number) - 2;    
+// }
+
+// alert(result);
