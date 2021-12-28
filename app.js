cc5958592f18be76d9a98b9b2a6828a9861b48d9
Panda <traitnil@gmail.com>
2021-12-28 20:24:14 +0200

    sayHelloTo

--- app.js
+++ app.js
@@ -1,38 +1,13 @@
-const a = 5;
-const b = 5; 
-const c = 30;
-const d = 5;  
-const e = 2;
-const f = 8;
-const numb = 33;
-const bool = false;
-const str = "javascript";
-const strNum = "100";
+const userName = prompt("Type your name, please");
 
-console.log(a * b);
-console.log(c / d);
-console.log(e + f);
-console.log(numb,bool,str,strNum);
+const sayHelloTo = function(name){
+    const alertText = "Hello, " + name;
+    alert(alertText);
+}
 
-let num = 1;
-num++;
-num--;
-num += 11;
-num -= 11;
-num *= 11;
-num /= 11;
+sayHelloTo(userName);
+
+sayHelloTo("Masha");
 
 
 
-
-// number = prompt('Type the number');
-
-// action = prompt('Choose your action');
-
-// if (action === '+') {
-// result = Number(number) + 2;
-// } else {
-// result = Number(number) - 2;    
-// }
-
-// alert(result);
